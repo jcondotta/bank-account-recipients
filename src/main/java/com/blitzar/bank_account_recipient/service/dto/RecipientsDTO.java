914e80aa475ec8c9311dbd0a7e2a5e759b2b0ed6
jcondotta <jefferson.condotta@gmail.com>
2024-09-18 16:21:32 +0200

    initial commit

--- src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientsDTO.java
+++ src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientsDTO.java
@@ -1,13 +1,11 @@
 package com.blitzar.bank_account_recipient.service.dto;
 
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
-@Introspected
 @Serdeable
 public record RecipientsDTO(Collection<RecipientDTO> recipients) {
 
--- src/main/java/com/blitzar/bank_account_recipient/web/controller/RecipientAPIConstants.java
+++ src/main/java/com/blitzar/bank_account_recipient/web/controller/RecipientAPIConstants.java
@@ -2,7 +2,8 @@ package com.blitzar.bank_account_recipient.web.controller;
 
 public interface RecipientAPIConstants {
 
-    String BASE_PATH_API_V1_MAPPING = "/api/v1/recipients/bank-account-id/{bank-account-id}";
-    String DELETE_RECIPIENT_API_V1_MAPPING = BASE_PATH_API_V1_MAPPING + "/recipient-name/{recipient-name}";
+    String BASE_PATH_API_V1_MAPPING = "/api/v1/recipients";
+    String BANK_ACCOUNT_API_V1_MAPPING = BASE_PATH_API_V1_MAPPING + "/bank-account-id/{bank-account-id}";
+    String DELETE_RECIPIENT_API_V1_MAPPING = BANK_ACCOUNT_API_V1_MAPPING + "/recipient-name/{recipient-name}";
 
 }
