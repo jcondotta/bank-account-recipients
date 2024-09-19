582267b3133be592e25c63c424372e7d324561a0
jcondotta <jefferson.condotta@gmail.com>
2024-09-19 13:46:34 +0200

    Adjusting logs for dev / aws environments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Remember to run mvn clean package before docker compose up, 
-    because docker container will need the jar file to start up and execute the lambda.
+    because docker container will need the jar file to start up and execute the lambda through local API gateway.
--- src/main/java/com/blitzar/bank_account_recipient/Application.java
+++ src/main/java/com/blitzar/bank_account_recipient/Application.java
@@ -1,11 +1,17 @@
 package com.blitzar.bank_account_recipient;
 
 import io.micronaut.context.env.Environment;
+import io.micronaut.context.env.PropertySource;
+import io.micronaut.logging.impl.LogbackLoggingSystem;
+import io.micronaut.logging.impl.LogbackUtils;
 import io.micronaut.runtime.Micronaut;
+
+import java.util.Map;
 
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("logback.configurationFile", "logback-dev.xml");
         Micronaut.build(args)
                 .mainClass(Application.class)
                 .defaultEnvironments(Environment.DEVELOPMENT)
