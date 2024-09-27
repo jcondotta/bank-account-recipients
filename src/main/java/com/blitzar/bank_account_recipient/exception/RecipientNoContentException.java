ce94162a7a699a2a378d21c3730fd58dc22acc1f
jcondotta <jefferson.condotta@gmail.com>
2024-09-27 10:28:16 +0200

    No Content 204 Exception Handler

---
+++ src/main/java/com/blitzar/bank_account_recipient/exception/RecipientNoContentException.java
@@ -0,0 +1,8 @@
+package com.blitzar.bank_account_recipient.exception;
+
+public class RecipientNoContentException extends RuntimeException {
+
+    public RecipientNoContentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/blitzar/bank_account_recipient/web/exception_handler/RecipientNoContentExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.blitzar.bank_account_recipient.web.exception_handler;
+
+import com.blitzar.bank_account_recipient.exception.RecipientNoContentException;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Produces
+@Singleton
+@Requires(classes = { RecipientNoContentException.class })
+public class RecipientNoContentExceptionHandler implements ExceptionHandler<RecipientNoContentException, HttpResponse<?>> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RecipientNoContentExceptionHandler.class);
+
+    private final ErrorResponseProcessor<?> errorResponseProcessor;
+
+    @Inject
+    public RecipientNoContentExceptionHandler(ErrorResponseProcessor<?> errorResponseProcessor) {
+        this.errorResponseProcessor = errorResponseProcessor;
+    }
+
+    @Override
+    @Status(value = HttpStatus.NO_CONTENT)
+    public HttpResponse<?> handle(HttpRequest request, RecipientNoContentException exception) {
+        return HttpResponse.noContent();
+    }
+}
