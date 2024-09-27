a1aae57dad0b5ac12b6f724e58e666c1965ef9c1
jcondotta <jefferson.condotta@gmail.com>
2024-09-27 10:27:44 +0200

    Purge recipients table helper class

---
+++ src/test/java/com/blitzar/bank_account_recipient/RecipientTablePurgeService.java
@@ -0,0 +1,35 @@
+package com.blitzar.bank_account_recipient;
+
+import com.blitzar.bank_account_recipient.domain.Recipient;
+import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+
+public class RecipientTablePurgeService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RecipientTablePurgeService.class);
+
+    private final DynamoDbTable<Recipient> dynamoDbTable;
+
+    @Inject
+    public RecipientTablePurgeService(DynamoDbTable<Recipient> dynamoDbTable) {
+        this.dynamoDbTable = dynamoDbTable;
+    }
+
+    public void purgeTable() {
+        logger.info("Purging all items from {} DynamoDB table", dynamoDbTable.tableName());
+
+        try {
+            var recipients = dynamoDbTable.scan()
+                    .items().stream()
+                    .toList();
+
+            recipients.forEach(dynamoDbTable::deleteItem);
+            logger.info("Successfully purged {} items.", recipients.size());
+        }
+        catch (Exception e) {
+            logger.error("Error purging recipient items from DynamoDB table", e);
+        }
+    }
+}
