058188995ba4e6c73816032970ab68157f2a0045
jcondotta <jefferson.condotta@gmail.com>
2024-09-27 10:28:32 +0200

    Adjusting recipients API's paths

--- src/main/java/com/blitzar/bank_account_recipient/web/controller/RecipientAPIConstants.java
+++ src/main/java/com/blitzar/bank_account_recipient/web/controller/RecipientAPIConstants.java
@@ -2,8 +2,9 @@ package com.blitzar.bank_account_recipient.web.controller;
 
 public interface RecipientAPIConstants {
 
-    String BASE_PATH_API_V1_MAPPING = "/api/v1/recipients";
-    String BANK_ACCOUNT_API_V1_MAPPING = BASE_PATH_API_V1_MAPPING + "/bank-account-id/{bank-account-id}";
+    String RECIPIENTS_BASE_PATH_API_V1_MAPPING = "/api/v1/recipients";
+    String BANK_ACCOUNT_API_V1_MAPPING = RECIPIENTS_BASE_PATH_API_V1_MAPPING + "/bank-account-id/{bank-account-id}";
+    String BANK_ACCOUNT_API_V1_PLACE_HOLDER = RECIPIENTS_BASE_PATH_API_V1_MAPPING + "/bank-account-id/%s";
     String RECIPIENT_NAME_API_V1_MAPPING = BANK_ACCOUNT_API_V1_MAPPING + "/recipient-name/{recipient-name}";
 
 }
