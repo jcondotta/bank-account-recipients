a6e61ba24cc4b91b02790b81bbfa5e2a8b27b6f5
jcondotta <jefferson.condotta@gmail.com>
2024-09-27 10:31:36 +0200

    Enhanced FetchRecipient structure

--- src/main/java/com/blitzar/bank_account_recipient/web/controller/FetchRecipientsController.java
+++ src/main/java/com/blitzar/bank_account_recipient/web/controller/FetchRecipientsController.java
@@ -2,15 +2,23 @@ package com.blitzar.bank_account_recipient.web.controller;
 
 import com.blitzar.bank_account_recipient.service.FetchRecipientService;
 import com.blitzar.bank_account_recipient.service.dto.RecipientsDTO;
+import com.blitzar.bank_account_recipient.service.request.QueryParams;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.*;
 import io.micronaut.validation.Validated;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.inject.Inject;
 
+import java.util.Optional;
 import java.util.UUID;
+
+import static io.swagger.v3.oas.annotations.media.Schema.*;
 
 @Validated
 @Controller(RecipientAPIConstants.BANK_ACCOUNT_API_V1_MAPPING)
@@ -23,9 +31,31 @@ public class FetchRecipientsController {
         this.fetchRecipientService = fetchRecipientService;
     }
 
-    @Get(produces = MediaType.APPLICATION_JSON)
-    public HttpResponse<?> findRecipients(@PathVariable("bank-account-id") UUID bankAccountId){
-        RecipientsDTO recipientsDTO = fetchRecipientService.findRecipients(bankAccountId);
+    @Status(HttpStatus.OK)
+    @Get(value = "{?queryParams*}", produces = MediaType.APPLICATION_JSON)
+    @Operation(summary = "Retrieve recipients for a bank account",
+            description = "Fetches recipients associated with a given bank account ID. Supports pagination and filtering by recipient name.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Recipients successfully retrieved",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = RecipientsDTO.class))),
+            @ApiResponse(responseCode = "204", description = "No recipients found for the provided bank account ID."),
+            @ApiResponse(responseCode = "400", description = "Invalid input or query parameters."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
+    public HttpResponse<?> findRecipients(
+            @Schema(description = "The unique identifier of the bank account.", requiredMode = RequiredMode.REQUIRED)
+            @PathVariable("bank-account-id") UUID bankAccountId,
+
+            @Schema(description = "Query parameters for filtering and pagination.", implementation = QueryParams.class)
+            @QueryValue("queryParams") Optional<QueryParams> queryParams) {
+
+        var recipientsDTO = fetchRecipientService.findRecipients(bankAccountId, queryParams.orElse(null));
+
+        if (recipientsDTO.recipients().isEmpty()) {
+            return HttpResponse.noContent();
+        }
+
         return HttpResponse.ok().body(recipientsDTO);
     }
 }
--- src/test/java/com/blitzar/bank_account_recipient/web/controller/FetchRecipientControllerIT.java
+++ src/test/java/com/blitzar/bank_account_recipient/web/controller/FetchRecipientControllerIT.java
@@ -1,52 +1,50 @@
 package com.blitzar.bank_account_recipient.web.controller;
 
-import com.blitzar.bank_account_recipient.LocalStackTestContainer;
-import com.blitzar.bank_account_recipient.exception.RecipientNotFoundException;
-import com.blitzar.bank_account_recipient.service.AddRecipientService;
-import com.blitzar.bank_account_recipient.service.dto.RecipientDTO;
+import com.blitzar.bank_account_recipient.*;
+import com.blitzar.bank_account_recipient.domain.Recipient;
 import com.blitzar.bank_account_recipient.service.dto.RecipientsDTO;
-import com.blitzar.bank_account_recipient.service.request.AddRecipientRequest;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 
 import java.time.Clock;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 
 @TestInstance(Lifecycle.PER_CLASS)
 @MicronautTest(transactional = false)
 public class FetchRecipientControllerIT implements LocalStackTestContainer {
 
+    private static final Logger logger = LoggerFactory.getLogger(FetchRecipientControllerIT.class);
+    private static final int PAGE_LIMIT_2 = 2;
+
     @Inject
-    private AddRecipientService addRecipientService;
+    private AddRecipientTestService addRecipientService;
+
+    @Inject
+    private DynamoDbTable<Recipient> dynamoDbTable;
 
     @Inject
     private Clock testFixedInstantUTC;
 
+    @Inject
+    private RecipientTablePurgeService recipientTablePurgeService;
+
+    private RecipientValidator recipientValidator;
     private RequestSpecification requestSpecification;
-
-    private final UUID bankAccountId = UUID.fromString("01920bff-1338-7efd-ade6-e9128debe5d4");
-
-    private final String recipientName1 = "Jefferson Condotta";
-    private final String recipientIBAN1 = "DE00 0000 0000 0000 00";
-
-    private final String recipientName2 = "Jefferson William";
-    private final String recipientIBAN2 = "ES00 0000 0000 0000 00";
-
 
     @BeforeAll
     public static void beforeAll(){
@@ -55,61 +53,173 @@ public class FetchRecipientControllerIT implements LocalStackTestContainer {
 
     @BeforeEach
     public void beforeEach(RequestSpecification requestSpecification) {
+        this.recipientValidator = new RecipientValidator();
         this.requestSpecification = requestSpecification
                 .contentType(ContentType.JSON)
                 .basePath(RecipientAPIConstants.BANK_ACCOUNT_API_V1_MAPPING);
     }
 
-    @Test
-    public void givenExistentRecipients_whenFetchRecipientsByBankAccountId_thenReturnOk(){
-        var addRecipientRequest1 = new AddRecipientRequest(recipientName1, recipientIBAN1);
-        var recipient1 = addRecipientService.addRecipient(bankAccountId, addRecipientRequest1);
-
-        var addRecipientRequest2 = new AddRecipientRequest(recipientName2, recipientIBAN2);
-        var recipient2 = addRecipientService.addRecipient(bankAccountId, addRecipientRequest2);
-
-        var expectedRecipients = List.of(recipient1, recipient2);
-
-        var recipientsDTO = given()
-            .spec(requestSpecification)
-                .pathParam("bank-account-id", bankAccountId)
-        .when()
-            .get()
-        .then()
-            .statusCode(HttpStatus.OK.getCode())
-                .extract()
-                    .as(RecipientsDTO.class);
-
-        assertThat(recipientsDTO.recipients()).hasSize(expectedRecipients.size());
-
-        expectedRecipients.stream().forEach(expectedRecipient -> {
-            RecipientDTO recipientDTO = recipientsDTO.recipients().stream()
-                    .filter(dto -> dto.name().equals(expectedRecipient.getName()))
-                    .findFirst()
-                    .orElseThrow(() -> new RecipientNotFoundException("recipient.notFound", bankAccountId, recipientName1));
-
-            assertAll(
-                    () -> assertThat(recipientDTO.name()).isEqualTo(expectedRecipient.getName()),
-                    () -> assertThat(recipientDTO.iban()).isEqualTo(expectedRecipient.getIban()),
-                    () -> assertThat(recipientDTO.bankAccountId()).isEqualTo(expectedRecipient.getBankAccountId()),
-                    () -> assertThat(recipientDTO.createdAt()).isEqualTo(LocalDateTime.now(testFixedInstantUTC))
-            );
-        });
+    @AfterEach
+    public void afterEach(){
+        recipientTablePurgeService.purgeTable();
     }
 
     @Test
-    public void givenNonExistentRecipients_whenFetchRecipientsByBankAccountId_thenReturnOk(){
-        var nonExistentBankAccountId = UUID.fromString("01920c09-6fe3-7780-8c2c-aee60a603f48");
+    void shouldReturn200OkAndRecipientList_whenBankAccountIdProvidedWithoutQueryParams() {
+        var expectedRecipients = addRecipientService.addRecipients(TestBankAccount.BRAZIL, TestRecipient.JEFFERSON, TestRecipient.INDALECIO);
+        addRecipientService.addRecipient(TestBankAccount.ITALY, TestRecipient.JESSICA);
+
         var recipientsDTO = given()
             .spec(requestSpecification)
+                .pathParam("bank-account-id", TestBankAccount.BRAZIL.getBankAccountId())
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.getCode())
+                    .body("recipients", hasSize(expectedRecipients.size()))
+                    .body("count", equalTo(expectedRecipients.size()))
+                    .body("lastEvaluatedKey", nullValue())
+                    .extract()
+                        .as(RecipientsDTO.class);
+
+        recipientValidator.validateRecipients(expectedRecipients, recipientsDTO.recipients());
+    }
+
+    @Test
+    void shouldReturn200OkAndQueryWithRecipientNamePrefix_whenRecipientNameIsProvided() {
+        var expectedRecipients = addRecipientService.addRecipients(TestBankAccount.BRAZIL, TestRecipient.JEFFERSON, TestRecipient.JESSICA);
+        addRecipientService.addRecipient(TestBankAccount.ITALY, TestRecipient.PATRIZIO);
+
+        final var prefixRecipientName = "Je";
+
+        var recipientsDTO = given()
+            .spec(requestSpecification)
+                .pathParam("bank-account-id", TestBankAccount.BRAZIL.getBankAccountId())
+                .queryParam("recipientName", prefixRecipientName)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.getCode())
+                    .body("recipients", hasSize(expectedRecipients.size()))
+                    .body("count", equalTo(expectedRecipients.size()))
+                    .body("lastEvaluatedKey", nullValue())
+                        .extract()
+                            .as(RecipientsDTO.class);
+
+        recipientValidator.validateRecipients(expectedRecipients, recipientsDTO.recipients());
+    }
+
+    @Test
+    void shouldReturn204NoContent_whenNonExistentBankAccountIdIsProvided() {
+        var nonExistentBankAccountId = TestBankAccount.BRAZIL.getBankAccountId().toString();
+
+        given()
+            .spec(requestSpecification)
                 .pathParam("bank-account-id", nonExistentBankAccountId)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.NO_CONTENT.getCode());
+
+        var pageIterable = dynamoDbTable.query(QueryConditional
+                .keyEqualTo(builder -> builder.partitionValue(nonExistentBankAccountId).build()));
+
+        assertThat(pageIterable.items()).isEmpty();
+    }
+
+    @Test
+    public void shouldReturn204NoContent_whenFilteringByNonExistentPrefixRecipientName(){
+        var jeffersonRecipientDTO = addRecipientService.addRecipient(TestBankAccount.BRAZIL, TestRecipient.JEFFERSON);
+
+        final var nonExistentPrefixRecipientName = "Z";
+        logger.debug("Searching for recipients with non-existent prefix name: {}", nonExistentPrefixRecipientName);
+
+        given()
+            .spec(requestSpecification)
+            .pathParam("bank-account-id", jeffersonRecipientDTO.bankAccountId())
+            .queryParam("recipientName", nonExistentPrefixRecipientName)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.NO_CONTENT.getCode());
+
+        final char existentPrefixRecipientName = jeffersonRecipientDTO.recipientName().charAt(0);
+        logger.debug("Searching for recipients with existent prefix name: {}", existentPrefixRecipientName);
+
+        given()
+            .spec(requestSpecification)
+            .pathParam("bank-account-id", jeffersonRecipientDTO.bankAccountId())
+            .queryParam("recipientName", existentPrefixRecipientName)
         .when()
             .get()
         .then()
             .statusCode(HttpStatus.OK.getCode())
-                .extract()
-                    .as(RecipientsDTO.class);
+            .body("recipients[0].recipientName", equalTo(jeffersonRecipientDTO.recipientName()));
+    }
 
-        assertThat(recipientsDTO.recipients()).hasSize(0);
+    @Test
+    public void shouldReturnPagedRecipients_whenLimitQueryParamIsSet_forSubsequentRequests(){
+        final var brazilBankAccountId = TestBankAccount.BRAZIL.getBankAccountId();
+
+        var expectedRecipientsPage1 = addRecipientService.addRecipients(brazilBankAccountId, TestRecipient.JEFFERSON, TestRecipient.INDALECIO);
+        var expectedRecipientsPage2 = addRecipientService.addRecipients(brazilBankAccountId, TestRecipient.JESSICA, TestRecipient.PATRIZIO);
+        var expectedRecipientsPage3 = addRecipientService.addRecipients(brazilBankAccountId, TestRecipient.VIRGINIO);
+
+        logger.debug("Fetching page 1 recipients for bank account ID: {}", brazilBankAccountId);
+        RecipientsDTO recipientsDTOPage1 = given()
+            .spec(requestSpecification)
+                .pathParam("bank-account-id", brazilBankAccountId)
+                .queryParam("limit", PAGE_LIMIT_2)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.getCode())
+                    .body("recipients", hasSize(expectedRecipientsPage1.size()))
+                    .body("count", equalTo(2))
+                    .body("lastEvaluatedKey.bankAccountId", equalTo(brazilBankAccountId.toString()))
+                    .body("lastEvaluatedKey.recipientName", equalTo(TestRecipient.JEFFERSON.getRecipientName()))
+                    .extract()
+                        .as(RecipientsDTO.class);
+
+        recipientValidator.validateRecipients(expectedRecipientsPage1, recipientsDTOPage1.recipients());
+
+        logger.debug("Fetching page 2 recipients for bank account ID: {} with last evaluated key: {}", brazilBankAccountId, TestRecipient.JEFFERSON.getRecipientName());
+        var recipientsDTOPage2 = given()
+            .spec(requestSpecification)
+                .pathParam("bank-account-id", brazilBankAccountId)
+                .queryParam("limit", PAGE_LIMIT_2)
+                .queryParam("lastEvaluatedKey.bankAccountId", brazilBankAccountId)
+                .queryParam("lastEvaluatedKey.recipientName", TestRecipient.JEFFERSON.getRecipientName())
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.getCode())
+                .body("recipients", hasSize(expectedRecipientsPage2.size()))
+                .body("count", equalTo(expectedRecipientsPage2.size()))
+                .body("lastEvaluatedKey.bankAccountId", equalTo(brazilBankAccountId.toString()))
+                .body("lastEvaluatedKey.recipientName", equalTo(TestRecipient.PATRIZIO.getRecipientName()))
+                    .extract()
+                        .as(RecipientsDTO.class);
+
+        recipientValidator.validateRecipients(expectedRecipientsPage2, recipientsDTOPage2.recipients());
+
+        logger.debug("Fetching page 3 recipients for bank account ID: {} with last evaluated key: {}", brazilBankAccountId, TestRecipient.PATRIZIO.getRecipientName());
+        var recipientsDTOPage3 = given()
+            .spec(requestSpecification)
+                .pathParam("bank-account-id", brazilBankAccountId)
+                .queryParam("limit", PAGE_LIMIT_2)
+                .queryParam("lastEvaluatedKey.bankAccountId", brazilBankAccountId)
+                .queryParam("lastEvaluatedKey.recipientName", TestRecipient.PATRIZIO.getRecipientName())
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.getCode())
+                .body("recipients", hasSize(expectedRecipientsPage3.size()))
+                .body("count", equalTo(expectedRecipientsPage3.size()))
+                .body("lastEvaluatedKey", nullValue())
+                    .extract()
+                        .as(RecipientsDTO.class);
+
+        recipientValidator.validateRecipients(expectedRecipientsPage3, recipientsDTOPage3.recipients());
     }
 }
