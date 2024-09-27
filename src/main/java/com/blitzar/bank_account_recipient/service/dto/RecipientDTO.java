554423b56228a8c1a5a9e27ab87f6a066779afa9
jcondotta <jefferson.condotta@gmail.com>
2024-09-27 10:33:11 +0200

    Recipients DTO open API annotation

--- src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientDTO.java
+++ src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientDTO.java
@@ -2,21 +2,43 @@ package com.blitzar.bank_account_recipient.service.dto;
 
 import com.blitzar.bank_account_recipient.domain.Recipient;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Introspected
+import static io.swagger.v3.oas.annotations.media.Schema.*;
+
 @Serdeable
+@Schema(name = "RecipientDTO", description = "Represents a recipient entity with account details.")
 public record RecipientDTO(
+
+        @Schema(description = "Unique identifier for the bank account.",
+                example = "f47ac10b-58cc-4372-a567-0e02b2c3d479",
+                requiredMode = RequiredMode.REQUIRED)
         UUID bankAccountId,
-        String name,
-        String iban,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC") LocalDateTime createdAt) {
+
+        @Schema(description = "Name of the recipient.",
+                example = "Jefferson Condotta",
+                maxLength = 30,
+                requiredMode = RequiredMode.REQUIRED)
+        String recipientName,
+
+        @Schema(description = "IBAN of the recipient.",
+                example = "GB29NWBK60161331926819",
+                maxLength = 34,
+                requiredMode = RequiredMode.REQUIRED)
+        String recipientIban,
+
+        @Schema(description = "Timestamp when the recipient was created.",
+                example = "2023-08-23T14:55:00Z",
+                requiredMode = RequiredMode.REQUIRED)
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+        LocalDateTime createdAt
+) {
 
     public RecipientDTO(Recipient recipient) {
-        this(recipient.getBankAccountId(), recipient.getName(), recipient.getIban(), recipient.getCreatedAt());
+        this(recipient.getBankAccountId(), recipient.getRecipientName(), recipient.getRecipientIban(), recipient.getCreatedAt());
     }
 }
--- src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientsDTO.java
+++ src/main/java/com/blitzar/bank_account_recipient/service/dto/RecipientsDTO.java
@@ -1,16 +1,35 @@
 package com.blitzar.bank_account_recipient.service.dto;
 
+import com.blitzar.bank_account_recipient.service.request.LastEvaluatedKey;
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
+import io.swagger.v3.oas.annotations.media.Schema;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
+
+import static io.swagger.v3.oas.annotations.media.Schema.*;
 
 @Serdeable
-public record RecipientsDTO(Collection<RecipientDTO> recipients) {
+@Schema(description = "A DTO representing a list of recipients with pagination details.")
+public record RecipientsDTO(
+
+        @Schema(description = "A list of recipient details.",
+                requiredMode = RequiredMode.REQUIRED)
+        List<RecipientDTO> recipients,
+
+        @Schema(description = "The total number of recipients returned.",
+                example = "25",
+                requiredMode = RequiredMode.REQUIRED)
+        int count,
+
+        @Schema(description = "The last evaluated key used for pagination, which allows fetching the next page of results.",
+                requiredMode = RequiredMode.NOT_REQUIRED)
+        LastEvaluatedKey lastEvaluatedKey
+) {
 
     @Override
-    public Collection<RecipientDTO> recipients() {
+    public List<RecipientDTO> recipients() {
         return Objects.nonNull(recipients) ? recipients : new ArrayList<>();
     }
 }
