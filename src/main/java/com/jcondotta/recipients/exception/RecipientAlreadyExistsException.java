ef37b7fa68067e2d92285a1d5826679720190416
jcondotta <jefferson.condotta@gmail.com>
2024-10-23 10:34:56 +0200

    Extracting some persitense logic into a recipientsRepository

---
+++ src/main/java/com/jcondotta/recipients/exception/RecipientAlreadyExistsException.java
@@ -0,0 +1,29 @@
+package com.jcondotta.recipients.exception;
+
+import java.util.UUID;
+
+public class RecipientAlreadyExistsException extends RuntimeException{
+
+    private final UUID bankAccountId;
+    private final String recipientName;
+
+    public RecipientAlreadyExistsException(String message, UUID bankAccountId, String recipientName) {
+        super(message);
+        this.bankAccountId = bankAccountId;
+        this.recipientName = recipientName;
+    }
+
+    public RecipientAlreadyExistsException(UUID bankAccountId, String recipientName) {
+        super();
+        this.bankAccountId = bankAccountId;
+        this.recipientName = recipientName;
+    }
+
+    public UUID getBankAccountId() {
+        return bankAccountId;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+}
