a81f25c1aa525dd7f82d31f091edc5a0384c37fb
jcondotta <jefferson.condotta@gmail.com>
2024-10-27 07:53:28 +0100

    Creating and testing WriteSyncCacheService

---
+++ src/main/java/com/jcondotta/recipients/service/cache/WriteSyncCacheService.java
@@ -0,0 +1,35 @@
+package com.jcondotta.recipients.service.cache;
+
+import com.jcondotta.recipients.service.dto.RecipientsDTO;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.json.JsonMapper;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@Singleton
+public class WriteSyncCacheService {
+
+    private static final Logger logger = LoggerFactory.getLogger(WriteSyncCacheService.class);
+
+    private final RedisCommands<String, RecipientsDTO> redisCommands;
+    private final Long cacheEntryExpirationInSeconds;
+
+    @Inject
+    public WriteSyncCacheService(RedisCommands<String, RecipientsDTO> redisCommands, @Value("${aws.sqs.endpoint:3600}") Long cacheEntryExpirationInSeconds) {
+        this.redisCommands = redisCommands;
+        this.cacheEntryExpirationInSeconds = cacheEntryExpirationInSeconds;
+    }
+
+    public void setCacheEntry(@NotNull RecipientsCacheKey recipientsCacheKey, @NotNull RecipientsDTO cacheValue) {
+        var cacheKey = recipientsCacheKey.getCacheKey();
+
+        redisCommands.setex(cacheKey, cacheEntryExpirationInSeconds, cacheValue);
+        logger.info("RecipientDTO cached with key: {}", cacheKey);
+    }
+}
---
+++ src/test/java/com/jcondotta/recipients/service/cache/WriteSyncCacheServiceTest.java
@@ -0,0 +1,74 @@
+package com.jcondotta.recipients.service.cache;
+
+import com.jcondotta.recipients.argument_provider.validation.query_params.QueryParamsArgumentProvider;
+import com.jcondotta.recipients.helper.TestBankAccount;
+import com.jcondotta.recipients.helper.TestRecipient;
+import com.jcondotta.recipients.service.dto.RecipientsDTO;
+import com.jcondotta.recipients.service.request.LastEvaluatedKey;
+import com.jcondotta.recipients.service.request.QueryParams;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class WriteSyncCacheServiceTest {
+
+    private static final UUID BANK_ACCOUNT_ID_BRAZIL = TestBankAccount.BRAZIL.getBankAccountId();
+    private static final Long DEFAULT_CACHE_ENTRY_EXPIRATION_IN_SECONDS = 3600L;
+
+    private WriteSyncCacheService writeSyncCacheService;
+
+    @Mock
+    private RedisCommands<String, RecipientsDTO> redisCommands;
+
+    @Mock
+    private RecipientsDTO recipientsDTO;
+
+    @BeforeEach
+    void beforeEach() {
+        writeSyncCacheService = new WriteSyncCacheService(redisCommands, DEFAULT_CACHE_ENTRY_EXPIRATION_IN_SECONDS);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(QueryParamsArgumentProvider.class)
+    void shouldSetCacheEntry_whenParametersAreValid(QueryParams queryParams) {
+        var recipientsCacheKey = new RecipientsCacheKey(BANK_ACCOUNT_ID_BRAZIL, queryParams);
+        writeSyncCacheService.setCacheEntry(recipientsCacheKey, recipientsDTO);
+
+        verify(redisCommands).setex(anyString(), anyLong(), any(RecipientsDTO.class));
+    }
+
+    @Test
+    void shouldSetCacheEntry_whenNoQueryParamsIsPassedInCacheKey() {
+        var recipientsCacheKey = new RecipientsCacheKey(BANK_ACCOUNT_ID_BRAZIL);
+        writeSyncCacheService.setCacheEntry(recipientsCacheKey, recipientsDTO);
+
+        verify(redisCommands).setex(anyString(), anyLong(), any(RecipientsDTO.class));
+    }
+
+    @Test
+    void shouldThrowNullPointerException_whenBankAccountIdCacheKeyIsNull() {
+        var exception = assertThrows(NullPointerException.class, () -> {
+            var recipientsCacheKey = new RecipientsCacheKey(null, new QueryParams());
+            writeSyncCacheService.setCacheEntry(recipientsCacheKey, recipientsDTO);
+        });
+
+        assertThat(exception)
+                .satisfies(violation -> assertThat(violation.getMessage())
+                        .isEqualTo("cache.recipients.bankAccountId.notNull"));
+    }
+}
