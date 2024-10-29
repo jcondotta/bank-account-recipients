ecd32a3125af1530b48f3b5ec60b4cb2bee9b62c
jcondotta <jefferson.condotta@gmail.com>
2024-10-29 22:21:50 +0100

    Adding Sonar Cloud analysis on github actions stack

--- src/main/java/com/jcondotta/recipients/validation/security/XSSPattern.java
+++ src/main/java/com/jcondotta/recipients/validation/security/XSSPattern.java
@@ -5,15 +5,15 @@ import java.util.regex.Pattern;
 public class XSSPattern implements ThreatInputPattern {
 
     private static final Pattern XSS_PATTERN = Pattern.compile(
-            "(<script.*?>.*?</script.*?>)|" +                // XSS <script> tag
+            "(<script[^>]*+>[^<]*+</script[^>]*+>)|" +       // XSS <script> tag
             "(%3C.*?script.*?%3E)|" +                        // Encoded <script>
             "(<img.*?onerror.*?>)|" +                        // XSS Image tag
             "(\".*?on.*?=.*?\")|" +                          // XSS Attribute injection
             "(javascript:.*?alert\\(.*?\\))|" +              // Inline JS execution
-            "(onload=alert\\(.*?\\))|" +                     // XSS event handler
+            "(onload=alert\\([^)]*+\\))|" +                     // XSS event handler
             "(<!--.*?<script.*?>.*?</script.*?>.*?-->)|" +   // XSS in HTML comments
             "(%3C.*?img.*?onerror.*?%3E)|" +                 // Encoded XSS in image tag
-            "(background-image:\\s*url\\(.*?javascript:.*?\\))" // XSS in CSS
+            "(background-image:\\s*+url\\([^)]*+javascript:[^)]*+\\))" // XSS in CSS
     );
 
     @Override
